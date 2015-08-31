474f12d4e332afb28f4c01455d3fd1e35e73165c
nbedelman <nedelman@g.harvard.edu>
2015-08-31 13:51:33 -0400

    use the agp file to create a chromosome-based fasta file

---
+++ scripts/scaffToChrom.py
@@ -0,0 +1,34 @@
+import sys
+import csv
+
+reference=open("Reference/Hmel2_2015-07-17/Hmel2.fa")
+count=0
+    
+refdict={}
+
+scafStarts={}
+now=0
+for scaffold in reference:
+	if '>' in scaffold:
+		name=scaffold.split()[0][1:]
+		length=int(scaffold.split()[1][7:-1])
+		scafStarts[name]=now
+		refdict[name]=''
+		now+=length
+	else:
+		refdict[name]+=(scaffold)
+
+chromDict={}
+for ag in agp:
+	chrom = ag.split()[0]
+	ND = ag.split()[4]
+	frag = ag.split()[5]
+	if chrom in chromDict.keys():
+		if ND=="D":
+		    chromDict[chrom]+=refdict[frag]
+		elif ND=="N":
+		    chromDict[chrom]+=("N"*100)
+		    
+chrFasta=open("Hmel2_chromosomes.fa","w")
+for key in chromDict.keys():
+    chrFasta.write('>'+key+' length='+str(len(chromDict[key]))+'\n'+chromDict.key)
